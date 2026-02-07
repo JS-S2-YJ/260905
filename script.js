a0d295844ad365aeb21ebe11f783dfa808ffa9bf
JS-S2-YJ <leejs1919@gmail.com>
2026-02-08 00:35:47 +0900

    fix error

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getFirestore, collection, addDoc, onSnapshot, query, orderBy } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 document.addEventListener('DOMContentLoaded', function() {
